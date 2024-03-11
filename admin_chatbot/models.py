7e74b05916bddb7d566231c96d946389ce6541c1
tribber93 <tribberyoni5@gmail.com>
2024-03-11 17:56:43 +0700

    add ingest app

--- admin_chatbot/models.py
+++ admin_chatbot/models.py
@@ -17,19 +17,3 @@ class FileUpload(models.Model):
             if os.path.exists(file_path):
                 os.remove(file_path)
         super().delete(*args, **kwargs)
-    
-    # def clean(self):
-    #     super().clean()
-
-    #     # Validasi ekstensi file
-    #     if not self.file_path.name.lower().endswith('.pdf'):
-    #         raise ValidationError({'file_path': 'Format file harus .pdf.'})
-
-    #     # Validasi ukuran file
-    #     max_size = 10 * 1024 * 1024  # 10MB
-    #     if self.file_path.size > max_size:
-    #         raise ValidationError({'file_path': 'Ukuran file tidak boleh lebih dari 10MB.'})
-
-    #     # Validasi nama file
-    #     if FileUpload.objects.exclude(pk=self.pk).filter(file_name=self.file_name).exists():
-    #         raise ValidationError('Nama file sudah ada.')
