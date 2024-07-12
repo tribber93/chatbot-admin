2ad668b1e8c00d2b572b3bf08726131aebbee8e0
tribber93 <tribberyoni5@gmail.com>
2024-07-13 04:08:59 +0700

    update fungsi get_top_dokumen_last_7_days()

--- admin_chatbot/functions.py
+++ admin_chatbot/functions.py
@@ -10,20 +10,24 @@ def get_top_dokumen_last_7_days():
     last_week = today - timezone.timedelta(days=6)  # Menghitung 6 hari yang lalu untuk mendapatkan rentang 7 hari terakhir
 
     # Query untuk mendapatkan data ChatHistory dari 7 hari terakhir
-    chat_data_last_7_days = ChatHistory.objects.filter(timestamp__range=[last_week, today])
+    chat_data_last_7_days = ChatHistory.objects.filter(timestamp__range=[last_week, today], is_answered=True)
 
     # Grupkan berdasarkan file_upload dan hitung jumlah kemunculannya, lalu urutkan secara descending
     top_document = chat_data_last_7_days.values('file_upload_id').annotate(count=Count('file_upload_id')).order_by('-count').first()
     
     # Jika tidak ada data dalam rentang waktu tersebut, kembalikan None atau pesan lain yang sesuai
     if not top_document:
-        top_document = {}
-        top_document["message"]= "Data Tidak Tersedia"
-        return top_document
+        return {"message": "Data Tidak Tersedia"}
         # return {"message": "Tidak ada dokumen dalam 7 hari terakhir"}
     
-    file_id = top_document['file_upload_id']
-    top_document['file_name'] = FileUpload.objects.get(id=file_id).file_name
+    try:
+        file_id = top_document['file_upload_id']
+        file_upload = FileUpload.objects.get(id=file_id)
+        top_document['file_name'] = file_upload.file_name
+    except FileUpload.DoesNotExist:
+        return {"message": "Data Tidak Tersedia"}
+    # file_id = top_document['file_upload_id']
+    # top_document['file_name'] = FileUpload.objects.get(id=file_id).file_name
     
     return top_document
 
