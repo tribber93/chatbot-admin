c758f9524cd68a878aa573175c5b0a7af4a197c5
tribber93 <tribberyoni5@gmail.com>
2024-07-13 12:23:22 +0700

    add command

--- rag_task/telegram_api.py
+++ rag_task/telegram_api.py
@@ -4,14 +4,23 @@ import requests
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 
+from admin_chatbot.models import FileUpload
 from rag_task.inference_function import chain_with_source, generate_chat
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 TELEGRAM_API_URL = f'https://api.telegram.org/bot{TELEGRAM_TOKEN}/'
-URL = "https://8105-180-251-229-58.ngrok-free.app/getpost/"
+URL = "https://b473-114-5-218-3.ngrok-free.app/getpost/"
 
 def setwebhook(request):
   response = requests.post(TELEGRAM_API_URL+ "setWebhook?url=" + URL).json()
+  
+  # Set commands
+  commands = [
+      {"command": "start", "description": "Mulai bot"},
+      {"command": "info", "description": "Dapatkan informasi tentang bot"}
+  ]
+  requests.post(TELEGRAM_API_URL + "setMyCommands", json={"commands": commands})
+  
   return HttpResponse(f"{response}")
 
 @csrf_exempt
@@ -26,13 +35,28 @@ def telegram_bot(request):
 def handle_update(update):
   chat_id = update['message']['chat']['id']
   text = update['message']['text']
-  chat_result = generate_chat(text, clean_response=True)
-#   print(chat_result)
-  send_message("sendMessage", {
-    'chat_id': chat_id,
-    'text': chat_result['answer'],
-    'parse_mode': 'Markdown',
-  })
+  if text == '/info':
+    top_5_file = FileUpload.objects.order_by('-count_retrieved')[:5]
+    str_top_5 = ""
+    for item in top_5_file:
+      str_top_5 += f"- {item.file_name} \n"
+    
+    answer = ("Kamu bisa bertanya tentang informasi akademik dan non-akademik."
+              "Berikut merupakan beberapa contoh hal yang paling sering ditanyakan.\n\n"
+              f"{str_top_5}")
+      
+    send_message("sendMessage", {
+        'chat_id': chat_id,
+        'text': answer,
+        'parse_mode': 'Markdown',
+    })
+  else:
+    chat_result = generate_chat(text, clean_response=True)
+    send_message("sendMessage", {
+      'chat_id': chat_id,
+      'text': chat_result['answer'],
+      'parse_mode': 'Markdown',
+    })
 
 def send_message(method, data):
   return requests.post(TELEGRAM_API_URL + method, data)
