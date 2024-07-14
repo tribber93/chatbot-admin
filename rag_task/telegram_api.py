6a612af16c1f52a5670cf94ad4e103759f4f259c
tribber93 <tribberyoni5@gmail.com>
2024-07-14 16:18:37 +0700

    update tele api

--- rag_task/telegram_api.py
+++ rag_task/telegram_api.py
@@ -1,15 +1,16 @@
 import os
 import json
+import markdown
 import requests
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
-
+from bs4 import BeautifulSoup
 from admin_chatbot.models import FileUpload
 from rag_task.inference_function import chain_with_source, generate_chat
 
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TEST_TOKEN")
 TELEGRAM_API_URL = f'https://api.telegram.org/bot{TELEGRAM_TOKEN}/'
-URL = "https://b473-114-5-218-3.ngrok-free.app/getpost/"
+URL = "https://3309-180-251-229-58.ngrok-free.app/getpost/"
 
 def setwebhook(request):
   response = requests.post(TELEGRAM_API_URL+ "setWebhook?url=" + URL).json()
@@ -36,13 +37,13 @@ def handle_update(update):
   text = update['message']['text']
   if text == '/info':
     top_5_file = FileUpload.objects.order_by('-count_retrieved')[:5]
-    str_top_5 = ""
-    for item in top_5_file:
-      str_top_5 += f"- {item.file_name} \n"
     
     answer = ("Kamu bisa bertanya tentang informasi akademik dan non-akademik."
               "Berikut merupakan beberapa contoh hal yang paling sering ditanyakan.\n\n"
-              f"{str_top_5}")
+              )
+    
+    for item in top_5_file:
+      answer += f"- {item.file_name} \n"
       
     send_message("sendMessage", {
         'chat_id': chat_id,
@@ -50,12 +51,20 @@ def handle_update(update):
         'parse_mode': 'Markdown',
     })
   else:
-    chat_result = generate_chat(text, clean_response=True)
+    chat_result = generate_chat(text)
+    answer = markdown_to_text(chat_result['answer'])
     send_message("sendMessage", {
       'chat_id': chat_id,
-      'text': chat_result['answer'],
-      'parse_mode': 'Markdown',
+      'text': answer,
+      # 'parse_mode': 'Markdown',
     })
 
 def send_message(method, data):
   return requests.post(TELEGRAM_API_URL + method, data)
+
+def markdown_to_text(markdown_string):
+    # Convert markdown to HTML
+    html = markdown.markdown(markdown_string)
+    # Use BeautifulSoup to parse the HTML and extract text
+    soup = BeautifulSoup(html, 'html.parser')
+    return soup.get_text()
